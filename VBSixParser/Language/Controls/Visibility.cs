54ea8053232bfee5b01fa3478b9a8a17c725e1b8
BulgPride <wsusoft@gmail.com>
2025-05-26 17:10:12 +0300

    Removing ignored files

--- VBSixParser/Language/Controls/Visibility.cs
+++
@@ -1,9 +0,0 @@
-// Namespace: VB6Parse.Language.Controls
-namespace VB6Parse.Language.Controls
-{
-    public enum Visibility
-    {
-        Hidden = 0,   // False
-        Visible = -1  // True (VB uses -1 for True)
-    }
-}
